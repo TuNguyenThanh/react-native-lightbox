0a465efca82217477a4de7afafbd95e4018f7493
Nguyễn Thanh Tú <thanhtu0903016975@gmail.com>
2018-03-02 16:24:27 +0700

    Update Lightbox.js

--- Lightbox.js
+++ Lightbox.js
@@ -1,6 +1,6 @@
 import React, { Component,  Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, TouchableHighlight, View } from 'react-native';
+import { Animated, TouchableOpacity, View } from 'react-native';
 
 import LightboxOverlay from './LightboxOverlay';
 
@@ -120,12 +120,13 @@ export default class Lightbox extends Component {
         onLayout={() => {}}
       >
         <Animated.View style={{opacity: this.state.layoutOpacity}}>
-          <TouchableHighlight
+          <TouchableOpacity
+            activeOpacity={1}
             underlayColor={this.props.underlayColor}
             onPress={this.open}
           >
             {this.props.children}
-          </TouchableHighlight>
+          </TouchableOpacity>
         </Animated.View>
         {this.props.navigator ? false : <LightboxOverlay {...this.getOverlayProps()} />}
       </View>
